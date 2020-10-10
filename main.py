802397df2f967f89ea12762c91cc7569b1f8432f
romarioGI <roma.fenc@yandex.ru>
2020-10-10 17:46:33 +0300

    __request

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
 import requests
 import vkApi
 
-baseUrl = "https://api.vk.com/method"
-method = "users.getSubscriptions"
-
-params = dict()
-params["user_id"] = "32745906"
-params["extended"] = "1"
-params["access_token"] = "3342c69f3342c69f3342c69fda333696f8333423342c69f6c3d984fabfee196ccc6e826"
-params["v"] = "5.124"
-
-print(vkApi.get_request_string(method, params))
+print(vkApi.get_user_subscriptions("1889345").content)
--- vkApi.py
+++ vkApi.py
@@ -1,7 +1,26 @@
-# constants, import from appconfig
+import requests
+
+# TODO: брать их лучше из типа appconfig
+# constants
 baseUrl = "https://api.vk.com/method"
+access_token = "3342c69f3342c69f3342c69fda333696f8333423342c69f6c3d984fabfee196ccc6e826"
+api_version = "5.124"
 
 
-def get_request_string(methodName: str, params: dict) -> str:
+def __request(methodName: str, params: dict):
     paramsString = "&".join(map(lambda p: p[0] + "=" + p[1], params.items()))
-    return baseUrl + "/" + methodName + "?" + paramsString
+    request_str = baseUrl + "/" + methodName + "?" + paramsString
+    return requests.get(request_str)
+
+
+# TODO: паблики это подмножество групп? нужно пробовать написать метод,
+#  возвращающий пересечение множеств идшников групп и сообществ
+# TODO: вытащить из json id-ки групп
+def get_user_subscriptions(user_id: str):
+    params = dict()
+    params["user_id"] = user_id
+    params["extended"] = "0"
+    params["access_token"] = access_token
+    params["v"] = api_version
+    method_name = "users.getSubscriptions"
+    return __request(method_name, params)
